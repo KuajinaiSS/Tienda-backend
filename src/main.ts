8eee97ab16f6de0d3fe88e10b2f1d1c91420027e
KuajinaiSS <kuajinaiss@gmail.com>
2025-07-14 00:33:14 -0400

    fix: actualizar el prefijo global de la API a /v3/api

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('/v2/api');
+  app.setGlobalPrefix('/v3/api');
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
